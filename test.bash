759b1d7fe09203a1706c28f22a00ae11f820b5ac
Iori Yamazaki <s22C1132PS@s.chibakoudai.jp>
2023-10-26 12:41:35 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Iori Yamazaki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
+
