a1dcd443f6c910ca6d07cfdfd9ac0bc86a5d3933
Iori Yamazaki <s22C1132PS@s.chibakoudai.jp>
2023-11-22 15:47:10 +0900

    Add a command

--- plus.bash
+++ plus.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2023 Yamazaki Iori s22C1132PS@s.chibakoudai.jp
 # SPDX-Licence-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res = 1
+}
+
+res = 0
